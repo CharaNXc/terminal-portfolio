2aa9c93058a8ca542b53e5443dbd49e70fea5f59
Charan Sai Anche <105865245+CharaNXc@users.noreply.github.com>
2024-12-31 21:22:24 +0530

    Update About.tsx

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -8,16 +8,18 @@ const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
       <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
-      </p>
-      <p>
-        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
-        Myanmar.
-      </p>
-      <p>
-        I am passionate about writing codes and <br />
-        developing web applications to solve real-life challenges.
-      </p>
+  Hi, my name is <HighlightSpan>Charan Sai Anche</HighlightSpan>!
+</p>
+<p>
+  I'm <HighlightAlt>a Cybersecurity and Software Engineer</HighlightAlt> based in Warangal, India.
+</p>
+<p>
+  I am passionate about <HighlightSpan>developing secure and scalable applications</HighlightSpan>, 
+  <br />
+  leveraging my expertise in <HighlightAlt>programming, cloud computing, and cybersecurity</HighlightAlt> to solve 
+  real-life challenges.
+</p>
+
     </AboutWrapper>
   );
 };
